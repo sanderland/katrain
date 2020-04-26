b188f233f29a1e8a095aff9c4ace87b074252e80
Sander Land <sander.land@cognite.com>
2020-04-26 23:41:02 +0200

    ai

--- ai2gtp.py
+++ ai2gtp.py
@@ -37,21 +37,15 @@ ai_settings["line_weight"] = 10
 
 ai_strategy = "P+Local"
 ai_settings["pick_frac"] = 0.0
-ai_settings["pick_n"] = 25
+ai_settings["pick_n"] = 20
 ai_settings["stddev"] = 1.5
 
 ai_strategy = "P+Pick"
 ai_settings["pick_frac"] = 0.33  # dropping below 7k
 ai_settings["pick_n"] = 5  # dropping below 7k at 5/0.33
 
-
 ai_strategy = "P+Weighted"
 ai_settings = {"pick_override": 0.95}
-
-ai_strategy = "P+Local"
-ai_settings["pick_frac"] = 0.0
-ai_settings["pick_n"] = 20
-ai_settings["stddev"] = 1.0
 
 
 logger.log(f"STARTED ENGINE", OUTPUT_ERROR)
