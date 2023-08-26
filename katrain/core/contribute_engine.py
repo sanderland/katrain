016b22dd4662e0038bcf7e7d6fd3000420ff8e55
Thaddée Tyl <thaddee.tyl@gmail.com>
2023-08-26 21:44:01 +0200

    Escape the distributed contribution command settings (#653)
    * update install.md by adding the missing ```. (#649)
    
    * Escape the distributed contribution command settings
    
    Fields passed to the distributed contribution command were unescaped,
    which caused some parameters (such as passwords) to close the command
    quote early.
    
    ---------
    
    Co-authored-by: hiuxia <98009342+hiuxia@users.noreply.github.com>

--- katrain/core/contribute_engine.py
+++ katrain/core/contribute_engine.py
@@ -72,7 +72,7 @@ class KataGoContributeEngine(BaseEngine):
         self.max_buffer_games = 2 * settings_dict["maxSimultaneousGames"]
         settings = {f"{k}={v}" for k, v in settings_dict.items()}
         self.command = shlex.split(
-            f'"{exe}" contribute -config "{cfg}" -base-dir "{base_dir}" -override-config "{",".join(settings)}"'
+            f'"{exe}" contribute -config "{cfg}" -base-dir "{base_dir}" -override-config {shlex.quote(",".join(settings))}'
         )
         self.start()
 
